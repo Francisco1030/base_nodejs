3c515d9807057842272fb880906283d774a06c9b
Francisco Viana <franciscoviana862@gmail.com>
2020-08-20 09:15:19 -0300

    10-demo;

---
+++ 10-multi-db-refactoring-mongodb/src/tests/postgresStrategy.test.js
@@ -0,0 +1,81 @@
+const assert = require('assert');
+const Postgres = require('../db/strategies/postgres');
+const Context = require('../db/strategies/base/contextStrategy');
+
+const context = new Context(new Postgres());
+const MOCK_HEROI_CADASTRAR = { 
+    nome: 'Gavião Negro',
+    poder: 'flexas'
+}
+
+const MOCK_HEROI_ATUALIZAR = { 
+    nome: 'Batman',
+    poder: 'Dinheiro'
+}
+
+describe('Postgres Strategy', function() {
+    this.timeout(Infinity);
+
+    this.beforeAll(async function() {
+        db = await context.connect();
+        await context.delete();
+        await context.create(MOCK_HEROI_ATUALIZAR);
+    });
+
+    it('PostgresSQL Connection', async function () {
+        const result = await context.isConnected();
+
+        assert.equal(result, true);
+    });
+
+    it('Cadatrar', async function() {
+        const result = await context.create(MOCK_HEROI_CADASTRAR);
+        delete result.id;
+
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+    });
+    it('Listar', async function() {
+        const [result] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome });
+        delete result.id;
+        //pegar a primeira posicao
+        //const posicaozero = resultado[0]
+        //const [posicao1, posicao2] = ['esse e o 1', 'esse e o 2'];
+        
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+    });
+    it('Atualizar', async function() {
+        const [itemAtualizar] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome });
+        const novoItem = {
+            ...MOCK_HEROI_ATUALIZAR,
+            nome: 'Mulher Maravilha'
+        }
+
+        const [result] = await context.update(itemAtualizar.id, novoItem);
+        const [itemAtualizado] = await context.read({ id: itemAtualizar.id });
+        assert.deepEqual(result, 1);
+        assert.deepEqual(itemAtualizado.nome, novoItem.nome);
+        /*
+        No JS temos uma tecnica chamada rest/spread que é um metodo para mergar obj ou separa-los
+        {
+            id:1,
+        }
+        
+        {
+            nome: 'Francisco',
+        }
+
+        finala
+        {
+            id: 1,
+            nome: 'Francisco',
+        }
+        */
+        
+    });
+    it('Remover por id', async function() {
+        const [item] = await context.read({});
+        const result = await context.delete(item.id);
+        
+        assert.deepEqual(result, 1);
+    });
+});
