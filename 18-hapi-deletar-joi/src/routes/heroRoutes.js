17b2bda2f92b57d84b24af461f1221bcfbb90b4d
Francisco Viana <franciscoviana862@gmail.com>
2020-08-27 17:19:03 -0300

    18-remover

---
+++ 18-hapi-deletar-joi/src/routes/heroRoutes.js
@@ -0,0 +1,121 @@
+const BaseRoute = require('./base/baseRoute');
+const Joi = require('joi');
+const failAction = (request, headers, error) => {
+    throw error;
+}
+
+class HeroRoutes extends BaseRoute {
+    constructor(db) {
+        super();
+        this.db = db;
+    }
+
+    list() {
+        return {
+            path: '/herois',
+            method: 'GET',
+            config: {
+                validate: {
+                    // payload -> body
+                    // headers -> header
+                    // params -> na URL :id
+                    // query -> ?skip=1&limit=100
+                    failAction: failAction,
+                    query: {
+                        skip: Joi.number().integer().default(0),
+                        limit: Joi.number().integer().default(10),
+                        nome: Joi.string().min(3).max(100)
+                    }
+                }
+            },
+            handler: (request, headers) => {
+                try {
+                    const { skip, limit, nome } = request.query;
+
+                    const query = nome ? { nome: { $regex: `.*${nome}*.` } } : {};
+
+                    return this.db.read(nome ? query : {}, skip, limit);
+                } catch (error) {
+                    console.log('deu ruim', error);
+                    return "Erro interno no servidor";
+                }
+            }
+        }
+    }
+
+    create() {
+        return {
+            path: '/herois',
+            method: 'POST',
+            config: {
+                validate: {
+                    failAction: failAction,
+                    payload: {
+                        nome: Joi.string().min(3).max(100).required(),
+                        poder: Joi.string().min(2).max(100).required(),
+                    }
+                }
+            },
+            handler: async (request, headers) => {
+                try {
+                    const { nome, poder } = request.payload;
+
+                    const result = await this.db.create({ nome, poder });
+
+                    return {
+                        message: 'Heroi cadastrado com sucesso!',
+                        _id: result._id
+                    };
+                } catch (error) {
+                    console.log('deu ruim', error);
+                    return "Erro interno no servidor";
+                }
+            }
+
+        }
+    }
+
+    update() {
+        return {
+            path: '/herois/{id}',
+            method: 'PATH',
+            config: {
+                validate: {
+                    failAction: failAction,
+                    params: {
+                        id: Joi.string().required()
+                    },
+                    payload: {
+                        nome: Joi.string().min(3).max(100),
+                        poder: Joi.string().min(2).max(100),
+                    }
+                }
+            },
+            handler: async (request, headers) => {
+                try {
+                    const { id } = request.params;
+                    const { payload } = request;
+
+                    const dadosString = JSON.stringify(payload);
+                    const dados = JSON.parse(dadosString);
+
+                    const result = await this.db.update(id, dados);
+
+                    if(result.nModified !== 1) return {
+                        message: 'Não foi possivel atualizar'
+                    }
+
+                    return {
+                        message: 'Heroi atualizado com sucesso!'
+                    };
+                } catch (error) {
+                    console.log('deu ruim', error);
+                    return "Erro interno no servidor";
+                }
+            }
+
+        }
+    }
+}
+
+module.exports = HeroRoutes;
