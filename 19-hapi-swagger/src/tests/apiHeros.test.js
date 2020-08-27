22abbe756256af3e7b3665302a938e45ef227010
Francisco Viana <franciscoviana862@gmail.com>
2020-08-27 20:27:47 -0300

    init doc

---
+++ 19-hapi-swagger/src/tests/apiHeros.test.js
@@ -0,0 +1,204 @@
+const assert = require('assert');
+const api = require('../api');
+let app = {};
+
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Hulk',
+    poder: 'verde'
+};
+
+const MOCK_HEROI_INICIAL = {
+    nome: 'Gavião Negro',
+    poder: 'mira'
+};
+
+let MOCK_ID = '';
+
+describe('Suite de testes da API Heroes', function () {
+    this.beforeAll(async () => {
+        app = await api;
+        const result = await app.inject({
+            method: 'POST',
+            url: '/herois',
+            payload: JSON.stringify(MOCK_HEROI_INICIAL)
+        });
+        const dados = JSON.parse(result.payload);
+        MOCK_ID = dados._id;
+    });
+
+    it('Listar /herois', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/herois?skip=0&limit=10'
+        });
+
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+
+        assert.deepEqual(statusCode, 200);
+        assert.ok(Array.isArray(dados));
+    });
+
+    it('Listar /herois deve retornar apenas 3 registros', async () => {
+        const TAMANHO_LIMITE = 3;
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        });
+
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados.length === TAMANHO_LIMITE);
+    });
+
+    it('Listar /herois deve retornar um erro com limite incorreto', async () => {
+        const TAMANHO_LIMITE = 'ESASAS';
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        });
+
+        const errorResult = {
+            "statusCode": 400,
+            "error": "Bad Request",
+            "message": "child \"limit\" fails because [\"limit\" must be a number]",
+            "validation": {
+                "source": "query",
+                "keys": ["limit"]
+            }
+        }
+
+        assert.deepEqual(result.statusCode, 400);
+
+        assert.deepEqual(result.payload, JSON.stringify(errorResult));
+    });
+
+    it('Listar /herois deve filtrar um item', async () => {
+        const NAME = 'Homem Aranha-1597871972102';
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=1000&nome=${NAME}`
+        });
+
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados[0].nome === NAME);
+    });
+
+    it('Cadatrar POST /herois ', async () => {
+        const result = await app.inject({
+            method: 'POST',
+            url: `/herois`,
+            payload: JSON.stringify(MOCK_HEROI_CADASTRAR)
+        });
+
+        const statusCode = result.statusCode;
+        const { message, _id } = JSON.parse(result.payload);
+
+        assert.ok(statusCode === 200);
+        assert.notStrictEqual(_id, undefined);
+        assert.deepEqual(message, 'Heroi cadastrado com sucesso!');
+    });
+
+    it('Atualizar PATH /herois/:id ', async () => {
+        const _id = MOCK_ID;
+        const experted = {
+            poder: 'Super mira'
+        };
+
+        const result = await app.inject({
+            method: 'PATH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(experted)
+        });
+
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+
+        assert.ok(statusCode === 200);
+        assert.deepEqual(dados.message, 'Heroi atualizado com sucesso!');
+    });
+
+    it('Atualizar PATH /herois/:id  - não deve atualizar com id incorreto', async () => {
+        const _id = `5f3d2a94733ef36b276f481b`;
+
+        const result = await app.inject({
+            method: 'PATH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify({
+                poder: 'Super mira'
+            })
+        });
+
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+
+        const experted = {
+            statusCode: 412,
+            error: 'Precondition Failed',
+            message: 'Id não encontrado no banco!'
+        };
+
+        assert.ok(statusCode === 412);
+        assert.deepEqual(dados, experted);
+    });
+
+    it('Remover DELETE /herois/:id ', async () => {
+        const _id = MOCK_ID;
+
+        const result = await app.inject({
+            method: 'DELETE',
+            url: `/herois/${_id}`,
+        });
+
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+
+        assert.ok(statusCode === 200);
+        assert.deepEqual(dados.message, 'Heroi removido com sucesso!');
+    });
+
+    it('Remover DELETE /herois/:id não deve remover', async () => {
+        const _id = '5f3d2a94733ef36b276f481b';
+
+        const result = await app.inject({
+            method: 'DELETE',
+            url: `/herois/${_id}`,
+        });
+
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+        const experted = {
+            statusCode: 412,
+            error: 'Precondition Failed',
+            message: 'Id não encontrado no banco!'
+        }
+
+        assert.ok(statusCode === 412);
+        assert.deepEqual(dados, experted);
+    });
+
+    it('Remover DELETE /herois/:id não deve remover com id invalido', async () => {
+        const _id = 'ID_INVALIDO';
+
+        const result = await app.inject({
+            method: 'DELETE',
+            url: `/herois/${_id}`,
+        });
+
+        const statusCode = result.statusCode;
+        const dados = JSON.parse(result.payload);
+        const experted = {
+            error: 'Internal Server Error',
+            message: 'An internal server error occurred',
+            statusCode: 500
+        }
+
+        assert.ok(statusCode === 500);
+        assert.deepEqual(dados, experted);
+    });
+});
