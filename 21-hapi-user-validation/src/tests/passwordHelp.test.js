7bf751916c238019fbe70a66c5de9143cd80850e
Francisco Viana <franciscoviana862@gmail.com>
2020-08-28 14:21:29 -0300

    teste de hash senha

--- 21-hapi-user-validation/src/tests/passwordHelp.test.js
+++ 21-hapi-user-validation/src/tests/passwordHelp.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
-const api = require('../api');
 
+const PasswordHelper = require('./../helpers/passwordHelper');
 const  SENHA = 'JotQuest@kuy#@';
+const HASH = '$2b$04$27NThhe6xig/GASS3qtHVeAO57jOyNx5dfVsxNvCnh/D2S.S3gHJy';
 
 describe.only('UserHelp test suite', function () {
     // this.beforeAll(async () => {
@@ -9,9 +10,14 @@ describe.only('UserHelp test suite', function () {
     // });
 
     it('deve gerar um hash a partir de uma senha', async () => {
-        const result = {};
+        const result = await PasswordHelper.hashPassword(SENHA);
 
-        assert.deepEqual(statusCode, 200);
         assert.ok(result.length > 10);
+    });
+
+    it('deve validar a senha', async () => {
+        const result = await PasswordHelper.comparePassword(SENHA, HASH);
+        
+        assert.ok(result);
     });
 });
